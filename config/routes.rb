fe1a5f8384db54f875b19e030dc19c98ddd23feb
osha7 <ogroetz@gmail.com>
2021-03-02 11:53:28 -0700

    changing ruby version for heroku2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   resources :nominated_movies
   post '/create-or-ignore-movie', to: "nominated_movies#create_or_ignore_movie"
-
   root to: 'application#home'
 
 end
